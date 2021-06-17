3581d41ca9c27104b9e16a9b30f038b15ec16bcd
Pol Feliu <feliupol@gmail.com>
2021-06-17 20:44:32 +0200

    separated counter widget to it's own

--- test/test.py
+++ test/test.py
@@ -4,20 +4,13 @@ from PyQt5.QtWidgets import *
 from pyreaqtive import *
 import sys
 
-class MainWindow(QMainWindow):
+class Counter(QWidget):
+
+    mycounter = RQInt(1)
+
     def __init__(self):
         super().__init__()
-
-        self.mycounter = RQInt(1)
-
-        self.bool_state = RQBool(True)
-
-        self.init_ui()
-
-    def init_ui(self):
-        self.main_widget = QWidget()
-        self.setCentralWidget(self.main_widget)
-        self.main_layout = QVBoxLayout(self.main_widget)
+        self.main_layout = QVBoxLayout(self)
 
         self.counter_increment_button = QPushButton("+")
         self.counter_increment_button.clicked.connect(lambda: self.mycounter.increment())
@@ -31,6 +24,24 @@ class MainWindow(QMainWindow):
 
         self.counter_display_label = RQLabel(self.mycounter)
         self.main_layout.addWidget(self.counter_display_label)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.bool_state = RQBool(True)
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.main_widget = QWidget()
+        self.setCentralWidget(self.main_widget)
+        self.main_layout = QVBoxLayout(self.main_widget)
+
+        self.counter = Counter()
+        self.main_layout.addWidget(self.counter)
+
 
         self.checkbox_1 = RQCheckbox(self.bool_state, "Checkbox 1")
         self.main_layout.addWidget(self.checkbox_1)
