62820ec552e97408312f0af9a7761dd8acb3f39e
Pol Feliu <plo.fc.123@gmail.com>
2021-07-10 16:44:23 +0200

    setup and license

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyreaqtive',
+    version='0.1',
+    packages=['pyreaqtive', 'pyreaqtive.models', 'pyreaqtive.widgets'],
+    url='https://github.com/polfeliu/pyreaqtive',
+    license='Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0)',
+    author='Pol Feliu Cuberes',
+    author_email='feliupol@gmail.com',
+    description='PyQt Reactive Framework'
+)
