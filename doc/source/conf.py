d88e2b0870e8b6dd7ce5111be0f73016cf7a4b18
Pol Feliu <plo.fc.123@gmail.com>
2021-07-17 18:14:43 +0200

    source follows logical order. Doc should too

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,6 +30,9 @@ extensions = [
     'sphinx.ext.autodoc'
 ]
 
+# Autodoc
+autodoc_member_order = 'bysource'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
