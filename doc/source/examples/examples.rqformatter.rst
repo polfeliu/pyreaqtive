4e0cbb559c98d5bfc6d7df328f08bf86361967bd
Pol Feliu <feliupol@gmail.com>
2021-07-20 20:42:53 +0200

    rqformatter example

---
+++ doc/source/examples/examples.rqformatter.rst
@@ -0,0 +1,10 @@
+RQFormatter
+==================================
+
+.. literalinclude:: ../../../examples/rqformatter.py
+   :caption: example code
+
+.. image:: ../_static/rqformatter.gif
+   :width: 200
+   :align: center
+ 
---
+++ examples/rqformatter.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from pyreaqtive.models import RQText, RQFormatter
+from pyreaqtive.widgets import RQLineEdit, RQLabel
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        widget = QWidget()
+        layout = QVBoxLayout()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        my_text = RQText("hello")
+
+        line_edit = RQLineEdit(my_text)
+        label = RQLabel(
+            RQFormatter(
+                "entered text is: {text}",
+                text=my_text
+            )
+        )
+
+        layout.addWidget(line_edit)
+        layout.addWidget(label)
+
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
