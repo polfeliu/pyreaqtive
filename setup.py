6ec1a7f00161e411e42113bbf3c2c8da02bbae90
Pol Feliu <feliupol@gmail.com>
2021-08-13 20:17:11 +0200

    publish description and version

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+from pyreaqtive import __version__
+
 setup(
     name='pyreaqtive',
-    version='0.1.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=__version__,
     packages=['pyreaqtive', 'pyreaqtive.models', 'pyreaqtive.widgets'],
     url='https://github.com/polfeliu/pyreaqtive',
     license='Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0)',
