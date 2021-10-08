95ccc17129688f588811f716f7dd8c5c95e125cd
Pol Feliu <feliupol@gmail.com>
2021-10-08 17:51:14 +0200

    check if already is reactive and improvements to example

--- examples/rq_getlist.py
+++ examples/rq_getlist.py
@@ -15,10 +15,15 @@ from typing import List
 class LibraryObject:
 
     def __init__(self):
+        # Be extracareful, this list will be replaced by a RQList.
+        # All list methods can be used by the library. But the type is not list anymore.
+        # The library cannot check against isinstance or issubclass
         self.my_list: List[str] = []
 
     def add_item(self):
-        self.my_list.append(str(randint(0, 10)))
+        self.my_list.extend(
+            [str(randint(0, 99)) for i in range(randint(1, 3))]
+        )
 
 
 class StringWidget(QLabel):
--- pyreaqtive/rq_getlist.py
+++ pyreaqtive/rq_getlist.py
@@ -1,10 +1,14 @@
-from typing import List
-
 from pyreaqtive import RQList
 
 
-def rq_getlist(obj: object, attribute_name: str):
-    # TODO Cache, check that is a list
-    rq_list = RQList(getattr(obj, attribute_name))
-    setattr(obj, attribute_name, rq_list)
-    return rq_list
+def rq_getlist(obj: object, attribute_name: str) -> RQList:
+    l = getattr(obj, attribute_name)
+    if isinstance(l, RQList):
+        # Already been reactivized
+        return l
+    elif not isinstance(l, list):
+        raise TypeError(f"{attribute_name} of obj is not list")
+    else:
+        rq_list = RQList(l)
+        setattr(obj, attribute_name, rq_list)
+        return rq_list
