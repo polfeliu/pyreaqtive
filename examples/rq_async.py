39e28a5c398561d1499ca830b672b55d7b9ab44f
Pol Feliu <feliupol@gmail.com>
2022-01-31 22:15:04 +0100

    started async example

---
+++ examples/rq_async.py
@@ -0,0 +1,64 @@
+import sys
+import requests
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+
+from pyreaqtive import RQList, RQVBoxLayout, RQLabel, RQCombobox, RQChoice
+
+
+def get_authors():
+    res = requests.get(f"https://poetrydb.org/authors")
+
+    return [Author(name) for name in res.json()['authors']]
+
+
+class Author:
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_author_titles(self):
+        res = requests.get(f"https://poetrydb.org/author/{self.name}/title")
+        return res.json()  # TODO
+
+    def __str__(self):
+        return self.name
+
+
+class Title:
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_text(self):
+        res = requests.get(f"https://poetrydb.org/title/{self.name}/lines")
+        return res.json()  # TODO
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        widget = QWidget()
+        layout = QVBoxLayout()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        self.author_list = RQList()
+        self.author_list.append(Author("asdf"))
+
+        self.author_selected = RQChoice(self.author_list, allow_none=True)
+
+        self.author_select = RQCombobox(self.author_selected)
+        layout.addWidget(self.author_select)
+
+        self.title_list = RQList()
+
+        self.title_selected = RQChoice(self.title_list, allow_none=True)
+
+        self.title_select = RQCombobox(self.title_selected)
+        layout.addWidget(self.title_select)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
