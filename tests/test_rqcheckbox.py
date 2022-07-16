122d776531e946ceae94cd04c7a74aa8710d10bb
Pol Feliu Cuberes <>
2022-07-16 21:47:18 +0200

    test

--- tests/test_rqcheckbox.py
+++ tests/test_rqcheckbox.py
@@ -1,27 +1,37 @@
 from pyreaqtive.models import RQBool
 from pyreaqtive.widgets import RQCheckBox
+import pytest_cases
 
 
-def test_rqbool(qtbot):
-    state = RQBool(False)
+@pytest_cases.parametrize("initial_state", [True, False])
+def test_rqbool(initial_state):
+    state = RQBool(initial_state)
 
     checkbox_1 = RQCheckBox(state, "checkbox 1")
     checkbox_2 = RQCheckBox(state, "checkbox 2")
 
-    # Initial state of the model is set to false
-    assert state.get() == False
-
-    assert checkbox_1.isChecked() == False
-    assert checkbox_2.isChecked() == False
+    # Initial state of the model is set to initial state
+    assert state.get() == initial_state
+    assert checkbox_1.isChecked() == initial_state
+    assert checkbox_2.isChecked() == initial_state
 
     # Toggle with checkbox 1
     checkbox_1.click()
-    assert checkbox_1.isChecked() == True
-    assert state.get() == True
-    assert checkbox_2.isChecked() == True
+    assert checkbox_1.isChecked() == (not initial_state)
+    assert state.get() == (not initial_state)
+    assert checkbox_2.isChecked() == (not initial_state)
 
     # Toggle with checkbox 2
     checkbox_2.click()
-    assert checkbox_2.isChecked() == False
-    assert state.get() == False
-    assert checkbox_1.isChecked() == False
+    assert checkbox_2.isChecked() == initial_state
+    assert state.get() == initial_state
+    assert checkbox_1.isChecked() == initial_state
+
+    state.toggle()
+    assert state.get() == (not initial_state)
+    assert checkbox_1.isChecked() == (not initial_state)
+    assert checkbox_2.isChecked() == (not initial_state)
+
+
+def test_rqcomputedbool():
+    pass  # state_1 = RQBool(False)
