9840e1d9a1ddd6c8ed5c9fd73b8bf1e55e17287f
Pol Feliu Cuberes <>
2022-07-24 15:53:05 +0200

    with direct variable

--- extract_coverage.py
+++ extract_coverage.py
@@ -1,19 +1,7 @@
 from xml.dom import minidom
-import os
 
-# parse an xml file by name
 file = minidom.parse('coverage.xml')
 
 percent = file.getElementsByTagName('coverage')[0].attributes['line-rate'].value
 percent = float(percent) * 100
-
-os.environ["COVERAGE_PERCENT"] = str(percent)
-
-color = "red"
-
-if percent == 100:
-    color = "brightgreen"
-elif percent > 90:
-    color = "orange"
-
-os.environ["COVERAGE_COLOR"] = color
+print(percent)
