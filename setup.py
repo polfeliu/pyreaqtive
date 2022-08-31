23746185604370191d16e3b55ce30ddf2e0d6456
Pol Feliu Cuberes <feliupol@gmail.com>
2022-08-31 21:02:10 +0200

    added typing marker

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 # read the contents of your README file
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -13,6 +14,7 @@ setup(
     long_description_content_type='text/markdown',
     version=__version__,
     packages=find_packages(include=["pyreaqtive", "pyreaqtive.*"]),
+    package_data={'pyreaqtive': ['py.typed']},
     url='https://github.com/polfeliu/pyreaqtive',
     license='Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0)',
     author='Pol Feliu Cuberes',
